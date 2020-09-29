0bb55cb2c4506cc8160be8c9b0b648ed708e22ef
melyaua <igor.melnychenko@tmx-learning.com>
2020-09-29 16:22:54 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        let str = String(n)
+        let newArr = str.split('').reverse().join('');
+        return Number(newArr)
+      }
+        let str = String(n).substring(1)
+        let newArr = str.split('').reverse().join('');
+        return Number(newArr)
 }
